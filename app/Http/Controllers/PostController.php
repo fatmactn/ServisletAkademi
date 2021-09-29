48cc8711334da2d88357c46206b329b13c6402f8
Ekrem Civan <e.civan5959@gmail.com>
2021-09-29 16:54:49 +0300

    First commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
